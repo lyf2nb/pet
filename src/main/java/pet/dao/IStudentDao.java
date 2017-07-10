dedadff3585f76bd0ee40e65cf561f6b39b08a1d
LiYuanFei <lyf_fbq@163.com>
2017-07-10 17:50:07 +0800

    有事情咨询
    Signed-off-by: LiYuanFei <lyf_fbq@163.com>

--- src/main/java/pet/dao/IStudentDao.java
+++ src/main/java/pet/dao/IStudentDao.java
@@ -1,10 +1,30 @@
 package pet.dao;
 
 import java.util.List;
-
+import org.apache.ibatis.annotations.Param;
 import pet.model.Student;
+import pet.model.StudentExample;
 
 public interface IStudentDao {
-    public List<Student> findStudenInfo();
+    long countByExample(StudentExample example);
 
+    int deleteByExample(StudentExample example);
+
+    int deleteByPrimaryKey(Integer studentId);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    List<Student> selectByExample(StudentExample example);
+
+    Student selectByPrimaryKey(Integer studentId);
+
+    int updateByExampleSelective(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByExample(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
 }
--- src/main/java/pet/model/Student.java
+++ src/main/java/pet/model/Student.java
@@ -1,50 +1,33 @@
 package pet.model;
 
 public class Student {
+    private Integer studentId;
 
     private String name;
-    private int age;
+
+    private String sex;
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
-        this.name = name;
+        this.name = name == null ? null : name.trim();
     }
-    public int getAge() {
-        return age;
+
+    public String getSex() {
+        return sex;
     }
-    public void setAge(int age) {
-        this.age = age;
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
     }
-    @Override
-    public String toString() {
-        return "Student [name=" + name + ", age=" + age + "]";
-    }
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + age;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Student other = (Student) obj;
-        if (age != other.age)
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
-    }
-    
 }
